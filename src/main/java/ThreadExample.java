3b37a273ac4a914781509935046445b32662f824
LukaszP <lukaszp4@onet.eu>
2019-07-19 08:33:28 +0200

    2nd commit

--- src/main/java/ThreadExample.java
+++ src/main/java/ThreadExample.java
@@ -1,8 +1,12 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadExample {
 
     public static void main(String args[]){
 
-        Thread t1 = new Thread(new Runnable() {
+      /*  Thread t1 = new Thread(new Runnable() {
             public void run() {
                 for(int i = 0; i < 999; i++){
                     System.out.println(Thread.currentThread().getName() + " " + i);
@@ -17,18 +21,27 @@ public class ThreadExample {
             }
         });
 
+        t1.setPriority(1);
+        t2.setPriority(10);
         t1.setName("t1");
-
         t2.setName("t2");
         t1.run();
         t2.run();
 
-
-
-
         //t1.run(); // run uruchamia zadanie w wątku głównym
-        //t1.start(); // uruchamia run() w osobnymm wątku
+        //t1.start(); // uruchamia run() w osobnymm wątku   */
 
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(()-> numberns());
+        executorService.submit(()-> numberns());
+        numberns();
+
+    }
+
+    private static void numberns() {
+        for (int i = 0; i < 999; i++){
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
     }
 
 }
